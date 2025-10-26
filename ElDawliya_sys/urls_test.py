2d442be6cc96a3ec623b7e3dd539da162c4f711c
7mohamed.gamal7 <7mohamed.gamal7@gmail.com>
2025-10-26 10:56:43 +0000

    تحديث جديد من قايربيسز

--- ElDawliya_sys/urls_test.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path, include
-
-urlpatterns = [
-    # Include HR API under a named namespace to satisfy reverse('hr_api:...')
-    path('', include(('Hr.api.urls', 'hr_api'), namespace='hr_api')),
-]
-
